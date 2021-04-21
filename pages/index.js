f07bb2c329ecba7504e6830c4cb4662480a76c98
Mishin Yura <mishin.inbox@gmail.com>
2021-04-21 14:15:58 +0300

    Added Regular React Components. Closes #4

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import {Fragment} from "react";
+import { getFeaturedEvents } from './dummy-data';
+import EventList from "../components/events/event-list";
 
 function HomePage() {
+    const featuredEvents = getFeaturedEvents();
 
-  return (
-    <Fragment>
-      <h1>The Home Page</h1>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <h1>The Home Page</h1>
+            <EventList items={featuredEvents}/>
+        </Fragment>
+    );
 }
 
 export default HomePage;
