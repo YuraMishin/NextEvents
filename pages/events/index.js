0e866e08e7618de7287ca11438092e3fa1e21da2
Mishin Yura <mishin.inbox@gmail.com>
2021-04-23 09:27:29 +0300

    Navigated to the Filtered Events Page Progammatically. Closes #12

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,18 +1,26 @@
-import {Fragment} from 'react';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
-import {getAllEvents} from '../../dummy-data';
+import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
-    const events = getAllEvents();
+  const router = useRouter();
+  const events = getAllEvents();
 
-    return (
-        <Fragment>
-            <EventsSearch/>
-            <EventList items={events}/>
-        </Fragment>
-    );
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
 export default AllEventsPage;
