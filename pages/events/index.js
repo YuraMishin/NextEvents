1137278612e93cfbef4ab5ae02c90d48078689a9
Mishin Yura <mishin.inbox@gmail.com>
2021-04-23 09:16:01 +0300

    Worked on the All Events Page. Closes #10

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,13 @@
 import {Fragment} from 'react';
+import {getAllEvents} from '../../dummy-data';
+import EventList from '../../components/events/event-list';
 
 function AllEventsPage() {
+    const events = getAllEvents();
+
     return (
         <Fragment>
-            <h1>All Events</h1>
+            <EventList items={events}/>
         </Fragment>
     );
 }
